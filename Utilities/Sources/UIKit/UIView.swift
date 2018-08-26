de94f2db28957b663649b4425cede7872e5f6809
Sven Bacia <sven.bacia@gmail.com>
2018-08-26 12:20:21 +0200

    fixes fill in layout guide parameters
    • the view parameter is not needed when specifing the layout guide

--- Utilities/Sources/UIKit/UIView.swift
+++ Utilities/Sources/UIKit/UIView.swift
@@ -37,10 +37,9 @@ public extension UIView {
     /// Fills the current view in the given view along the layout guide with optional edge insets.
     ///
     /// - Parameters:
-    ///   - view: The containing view.
     ///   - layoutGuide: The layout guide.
     ///   - inset: An optional inset.
-    public func fill(`in` view: UIView, layoutGuide: UILayoutGuide, inset: NSDirectionalEdgeInsets = .zero) {
+    public func fill(`in` layoutGuide: UILayoutGuide, inset: NSDirectionalEdgeInsets = .zero) {
         topAnchor.constraint(equalTo: layoutGuide.topAnchor, constant: inset.top).isActive = true
         leadingAnchor.constraint(equalTo: layoutGuide.leadingAnchor, constant: inset.leading).isActive = true
         bottomAnchor.constraint(equalTo: layoutGuide.bottomAnchor, constant: inset.bottom).isActive = true
